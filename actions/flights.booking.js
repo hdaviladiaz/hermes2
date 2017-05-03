32b505a3a66640ab6f9e1613ba9506354044a561
hector <hdavila@thoughtworks.com>
2017-05-03 14:14:21 -0500

    test4

--- actions/flights.booking.js
+++ actions/flights.booking.js
@@ -1,20 +1,21 @@
 var searchFlightService = require('../services/searchFlightService.js');
-
+var flightUserInfo = require('../actions/flights.info.js')
 
 var execute = function (request) {
+
     var parameters = request.result.contexts[0].parameters;
     var from = parameters.from;
     var to = parameters.to;
     var date = parameters.date;
     var numbers = parameters.numbers;
+
     var name = parameters.name;
     var lastname = parameters.lastname;
     var passport = parameters.passport;
+
     if (!name || !lastname || !passport) {
-        var event = searchFlightService.createEventRequest("flights.booking", parameters);
-        return {
-            event: event
-        }
+        var event = flightUserInfo.createEventRequest("flights.booking", parameters);
+        return event;
     }
     var result = searchFlightService.bookFlight(from, to, date, numbers, name, lastname, passport);
     var response = {
