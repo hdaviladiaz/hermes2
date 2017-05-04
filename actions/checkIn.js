098a9dc1299bfef9003128929a4a63d23b31c1b5
hector <hdavila@thoughtworks.com>
2017-05-04 16:13:37 -0500

    test3

--- actions/checkIn.js
+++ actions/checkIn.js
@@ -4,8 +4,8 @@ var execute = function (request) {
 
     var parameters = request.result.parameters;
     var passport = parameters.passport;
-    console.log(passport);
-    var reservation = reservationService.getReservation(passport);
+    console.log("passport=",passport);
+    var reservation = reservationService.getReservation(passport.join(''));
     var text = "Dou you want a specific seat?";
     if (!reservation || reservation.length == 0) {
         text = "You dont have any flight to check in.";
