1fbe670004e9ec4d15b491de4fcd35d6c0294ec7
hector <hdavila@thoughtworks.com>
2017-05-04 16:05:35 -0500

    test2

--- model/reservation.js
+++ model/reservation.js
@@ -5,7 +5,7 @@ function Reservations() {
     var duration = 60 + (i * 20);
     options.push({
       "flightId": "LT" + (i + 1) + "F" + (i + 1) * 100,
-      "passport": "123456",
+      "passport": "one two three four five six",
       "price": 500 + i * 50,
       "class": i % 2 == 0 ? "first" : "tourist",
       "duration": "2h",
