bc8122d9a4c7466ebcf5b0b1e79dea2f97156448
ymarinTw <ymarin@thoughtworks.com>
2017-05-17 16:46:20 -0500

    servicios flight status

--- gestor/statusFlightGestor.js
+++ gestor/statusFlightGestor.js
@@ -1,27 +1,5 @@
 var _ = require('underscore');
 
-var statusFlight = function (airlineCode, flightNumber, date) {
-    var Client = require('node-rest-client').Client;
-    var client = new Client();
-    client.get("https://www.latam.com/ws/api/flight-status/1.1/rest/airline-code/" + airlineCode + "/flight-number/" + flightNumber + "/standar-time-condition/SCHEDULE/departure-date/" + date + "/language/EN/country/US", function (data, response) {
-        if (data.data != null) {
-            var flights = data.data.flights;
-            if (flights && flights.length != 0) {
-                var legs = flights[0].legs;
-                if (legs && legs.length != 0) {
-                    var obj = legs[0];
-                    var result = {status: obj.descriptionStatusCode, datetime: null};
-                    if (obj.ontime == 'ontime' && obj.departure != null && obj.departure.blockOff != null && obj.departure.blockOff.scheduleDateTime != null){
-                        result.datetime = obj.departure.blockOff.scheduleDateTime;
-                    }
-                    return result;
-                }
-            }
-        }
-        return null;
-    });
-}
-
 var statusFlightHttpRequest = function (airlineCode, flightNumber, date) {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xhr = new XMLHttpRequest();
@@ -29,10 +7,21 @@ var statusFlightHttpRequest = function (airlineCode, flightNumber, date) {
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send();
     var response = JSON.parse(xhr.responseText);
-    console.log(response);
+    if (response.data != null) {
+        var flights = response.data.flights;
+        if (flights && flights.length != 0) {
+            var legs = flights[0].legs;
+            if (legs && legs.length != 0) {
+                var obj = legs[0];
+                var result = {status: obj.descriptionStatusCode, datetime: null};
+                if (obj.flightStatus == 'ontime' && obj.departure != null && obj.departure.blockOff != null && obj.departure.blockOff.scheduleDateTime != null){
+                    result.datetime = obj.departure.blockOff.scheduleDateTime;
+                }
+                return result;
+            }
+        }
+    }
     return null;
 }
 
 exports.statusFlightHttpRequest = statusFlightHttpRequest;
-exports.statusFlight = statusFlight;
-
--- services/statusFlightService.js
+++ services/statusFlightService.js
@@ -3,15 +3,14 @@ var i18n = require('../i18n');
 
 var getStatus = function (airlineCode, flightNumber, date) {
   var flight = statusFlightGestor.statusFlightHttpRequest(airlineCode, flightNumber, date);
-  console.log(flight);
   var text = "";
   if (flight == null) {
     text = __('flights.notfound');
   } else {
     text = __('flights.flightis') + flight.status;
     if (flight.datetime != null) {
-      var mins = (Date.now - Date.parse(flight.datetime)) / 60000;
-      text += __('flights.takeoff') + mins + __('minutes');
+      var mins = (Date.parse(flight.datetime.local) - Date.now()) / 60000;
+      text += __('flights.takeoff') + Math.round(mins, 0) + __('minutes');
     }
   }
   
