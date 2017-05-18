d533d26d78d389fd5fed867512cf3027e36470dc
ymarinTw <ymarin@thoughtworks.com>
2017-05-18 11:14:33 -0500

    translate flight.status responses to spanish

--- actions/flight.status.js
+++ actions/flight.status.js
@@ -2,8 +2,8 @@ var statusFlightService = require('../services/statusFlightService.js');
 var moment = require('moment');
 
 var execute = function (request) {
-    var date = moment(moment().add(1, "days"), "YYYY-MM-DD").format();
-    var text = statusFlightService.getStatus("LA", "1449", date);
+    var date = moment(moment().add(1, "minutes"), "YYYY-MM-DD").format();
+    var text = statusFlightService.getStatus("LA", "1451", date);
     return {
         text: text
     }
--- services/statusFlightService.js
+++ services/statusFlightService.js
@@ -11,8 +11,20 @@ var getStatus = function (airlineCode, flightNumber, date) {
   } else {
     text = __('flights.flightis') + flight.status;
     if (flight.datetime != null) {
-      var relTime = humanize.relativeTime(moment(flight.datetime.local).unix());
-      console.log(moment(flight.datetime.local).unix());
+      var relTime = humanize.relativeTime(moment("2017-05-20T13:15:00").unix());
+      //var relTime = humanize.relativeTime(moment(flight.datetime.local).unix());
+      
+      if (getLocale().toLowerCase() == "es") {
+        relTime = relTime.replace("just now","ahora mismo");
+        relTime = relTime.replace("now","ahora mismo");
+        relTime = relTime.replace("second","segundo");
+        relTime = relTime.replace("minute","minuto");
+        relTime = relTime.replace("hour","hora");
+        relTime = relTime.replace("day","día");
+        relTime = relTime.replace("in about an","1");
+        relTime = relTime.replace("in about a","1");
+        relTime = relTime.replace("in ","en ");
+      }
       text += __('flights.takeoff') + relTime;
     }
   }
